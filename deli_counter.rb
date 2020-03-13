aad7e57ef6f90d84e68efccffd59811de6381c34
Charles Butler <cdbutle@outlook.com>
2020-03-13 01:09:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,11 @@ def line(katz_deli)
   array = []
   if array.size == 0
     puts "There is nobody waiting to be served!"
-  end
+  else
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |name, index| 
+      current_line << " #{index}. #{name}"
+    end
+    puts current_line
+    end
 end
-
-def
